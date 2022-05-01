a68122b9e2d5d7b22cefe97782ebc2688b8fd2aa
rafalbaldyga <rafalbaldyga20@gmail.com>
2022-05-02 01:04:03 +0200

    Add files via upload

---
+++ bill split calculator.py
@@ -0,0 +1,15 @@
+
+print("Welcome to the bill-split calculator!")
+
+bill1 = float(input("What was the total bill? "))
+
+percent = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
+
+people1 = float(input("How many people to split the bill? "))
+
+
+z = bill1/people1 + (bill1*(percent/100))
+
+y = round(z, 2)
+
+print("Each person should pay: " + f"{y}")
