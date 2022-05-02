94ee7914ebacbd832a46d89c5bc7459256a76fe3
rafalbaldyga <rafalbaldyga20@gmail.com>
2022-05-02 21:16:27 +0200

    Update bill split calculator.py

--- bill split calculator.py
+++ bill split calculator.py
@@ -7,9 +7,9 @@ percent = float(input("What percentage tip would you like to give? 10, 12 or 15?
 
 people1 = float(input("How many people to split the bill? "))
 
-
 z = bill1/people1 + (bill1*(percent/100))
 
+#Total amount, round to two decimal places
 y = round(z, 2)
 
 print("Each person should pay: " + f"{y}")
